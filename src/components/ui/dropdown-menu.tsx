32acf63734f4b7558a38409d4e8917c77cb08d5c
Brandegee Pierce <brandegee@pirani.life>
2025-10-19 23:47:40 -0400

    Fix RadioItem missing value prop

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -90,9 +90,10 @@ const DropdownMenuCheckboxItem = React.forwardRef<any, any>(({ className, childr
 DropdownMenuCheckboxItem.displayName =
   DropdownMenuPrimitive.CheckboxItem.displayName
 
-const DropdownMenuRadioItem = React.forwardRef<any, any>(({ className, children, ...props }, ref) => (
+const DropdownMenuRadioItem = React.forwardRef<any, any>(({ className, children, value, ...props }, ref) => (
   <DropdownMenuPrimitive.RadioItem
     ref={ref}
+    value={value}
     className={cn(
       "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className
